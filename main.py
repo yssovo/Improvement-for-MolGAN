33157c599b522795666202a44c4921a7ebb7a0ae
yssovo <yssovo@postech.ac.kr>
2022-05-27 02:09:00 +0900

    some fix

--- main.py
+++ main.py
@@ -49,8 +49,8 @@ if __name__ == '__main__':
 
     # Training configuration.
     parser.add_argument('--batch_size', type=int, default=16, help='mini-batch size')
-    parser.add_argument('--num_iters', type=int, default=200000, help='number of total iterations for training D')
-    parser.add_argument('--num_iters_decay', type=int, default=100000, help='number of iterations for decaying lr')
+    parser.add_argument('--num_iters', type=int, default=10000, help='number of total iterations for training D')
+    parser.add_argument('--num_iters_decay', type=int, default=5000, help='number of iterations for decaying lr')
     parser.add_argument('--g_lr', type=float, default=0.0001, help='learning rate for G')
     parser.add_argument('--d_lr', type=float, default=0.0001, help='learning rate for D')
     parser.add_argument('--dropout', type=float, default=0., help='dropout rate')
